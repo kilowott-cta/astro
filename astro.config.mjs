567fd43be2b21cf2d7bfe189a29dfffc24359869
kilowott cta <gilroy.menezes@kilowott.com>
2024-08-29 15:57:04 +0530

    GitHub publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astronaut.github.io",
+  site: "https://kilowott-cta.github.io",
+  base: "/dreary-debris"
 });
