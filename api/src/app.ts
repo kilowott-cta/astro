f12b55027fbcfe64d9b8023f86ad9404040dca48
kilowott cta <gilroy.menezes@kilowott.com>
2024-11-01 16:09:43 +0530

    Changed back from Hono to default

--- api/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/naut", (c) => c.text("Hello Azure Functions!"));
-
-export default app;
---
+++ api/src/functions/user.ts
@@ -0,0 +1,20 @@
+import { app, HttpRequest, HttpResponse, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+let user = null;
+
+async function getUser(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log('Http function was triggered.');
+
+    const mock = await fetch('https://randomuser.me/api/');
+    const data = await mock.json();
+    user = data.results[0];
+
+    const response = new HttpResponse({ jsonBody: user });
+    response.headers.set('content-type', 'application/json');
+    return response;
+};
+
+app.http('user', {
+    methods: ['GET', 'POST'],
+    handler: getUser
+});
