d9683604e6dca5412fc2756cda98ce7bff304c59
kilowott cta <gilroy.menezes@kilowott.com>
2024-11-03 16:12:26 +0530

    Fulldev for astro components and blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+import fulldev from 'fulldev-ui/integration';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [
+    svelte(), 
+    tailwind(), 
+    fulldev({
+      colors: {
+        theme: 'dark',
+        dark: {
+          background: '#111110',
+          base: '#6F6D66',
+          brand: '#F50',
+        },
+        light: {
+          background: '#EEEEEC',
+          base: '#6F6D66',
+          brand: '#F50'
+        }
+      }
+    })
+  ]
 });
